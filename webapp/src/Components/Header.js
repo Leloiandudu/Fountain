8e2686252856d57758e516875ee8203b0cc62811
Leloiandudu <leloiandudu@gmail.com>
2016-08-21 14:12:57 +1200

    some ui fixes

--- webapp/src/Components/Header.js
+++ webapp/src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import WikiLink from './WikiLink';
+import Link from './Link';
 import url from '../url';
 
 export default ({children}) => (
    <div className='Header'>
       <div className='mainContentPane'>
-         <img className='logo' src={url('/Content/logo.png')} />
+         <Link to='/'><img className='logo' src={url('/Content/logo.png')} /></Link>
          {Global.user && <div className='login'>
             <span className='userName'><WikiLink to={`U:${Global.user.name}`} /></span>
             <a className='action' href={url(`/logout?redirectTo=${window.location.pathname}`)}>Выйти</a>
