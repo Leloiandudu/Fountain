c15b46c3326a453bc21ec61539adbbffac8459d6
Leloiandudu <leloiandudu@gmail.com>
2019-12-25 14:24:54 +1300

    Switch to TLS 1.2

--- WikiFountain.Server/Global.asax.cs
+++ WikiFountain.Server/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -22,6 +23,7 @@ namespace WikiFountain.Server
 
         void Application_Start(object sender, EventArgs e)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             AreaRegistration.RegisterAllAreas();
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             MvcConfig.Register();
