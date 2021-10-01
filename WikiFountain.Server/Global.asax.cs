3621ffe08c8751bd691c626563bd7724e3c92631
Leloiandudu <leloiandudu@gmail.com>
2021-10-01 23:23:05 +1300

    fix LetsEncrypt issues

--- WikiFountain.Server/Global.asax.cs
+++ WikiFountain.Server/Global.asax.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -12,8 +15,27 @@ namespace WikiFountain.Server
     {
         public static void BeforeStart()
         {
+            ServicePointManager.ServerCertificateValidationCallback = LetsEncryptWorkaround;
             AppDomain.CurrentDomain.UnhandledException += (s, e) => Log(e.ExceptionObject.ToString());
             Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(DbSessionModule));
+        }
+
+        private static readonly string[] Chain = new[]
+        {
+            "A053375BFE84E8B748782C7CEE15827A6AF5A405",
+            "933C6DDEE95C9C41A40F9F50493D82BE03AD87BF",
+            "DAC9024F54D8F6DF94935FB1732638CA6AD77C13",
+        };
+
+        static bool LetsEncryptWorkaround(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            if (sslPolicyErrors != SslPolicyErrors.RemoteCertificateChainErrors)
+                return false;
+
+            return chain.ChainElements.Cast<X509ChainElement>().Skip(1).Select(c => c.Certificate.GetCertHashString()).SequenceEqual(Chain);
         }
 
         public static void Log(string text)
